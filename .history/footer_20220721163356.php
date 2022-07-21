63f7c98f27356419285a0f201e844a17547804ed
Michelhof1978 <michel.hof@hotmail.fr>
2022-07-21 19:00:13 +0200

    commit11

---
+++ .history/footer_20220721163356.php
@@ -0,0 +1,26 @@
+<!--Footer du site-->
+
+<div >
+<div>
+<a href="https://www.linkedin.com/in/michel-h-245436203/">
+<img src="images/linkedinLogo.png" alt="Profil Linkedin" title="Profil Linkedin" /> 
+</a>
+</div>
+
+<div>
+<a href="https://github.com/Michelhof1978">
+<img src="images/githubLogo.png" alt="Projets Github" title="Projets Github" /> 
+</a>
+</div>
+
+<div class="text-center colorWhite bg-colorBlue3">
+  Tous droits réservés
+</div>
+
+<div class="compteur">
+<a href="http://www.mon-compteur.fr"><img src="http://www.mon-compteur.fr/html_c02genv2-74909-2" border="0"/></a>
+</div>
+</div>
+<script src="menu.js"></script>
+</body>
+</html>
