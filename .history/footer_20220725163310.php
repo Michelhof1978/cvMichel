7c0ce05e1fab7888336025e38df61163c2cc5fd7
Michelhof1978 <michel.hof@hotmail.fr>
2022-07-25 17:37:53 +0200

    commit12

---
+++ .history/footer_20220725163310.php
@@ -0,0 +1,32 @@
+<!--Footer du site-->
+<footer class="footer text-center colorWhite bg-colorBlue3 height:30 px">
+
+
+<a href="https://www.linkedin.com/in/michel-h-245436203/">
+<img src="images/linkedinLogo.png" alt="Profil Linkedin" title="Profil Linkedin" /> 
+</a>
+
+&nbsp&nbsp&nbsp&nbsp&nbsp
+
+<a href="images/cv-HoffmannMichel.pdf" download>
+<img src="images/cv.png" alt="Telechargement Cv" title="Telechargement Cv" /> 
+</a>
+
+
+&nbsp&nbsp&nbsp&nbsp&nbsp
+<a href="https://github.com/Michelhof1978">
+<img src="images/github.png" alt="Projets Github" title="Projets Github" /> 
+</a>
+
+
+<div>
+  Tous droits réservés
+  </div>
+
+</footer>
+
+
+
+<script src="menu.js"></script>
+</body>
+</html>
---
+++ .history/footer_20220725164820.php
@@ -0,0 +1,36 @@
+<!--Footer du site-->
+<footer class="footer text-center colorWhite bg-colorBlue3 height:30 px">
+
+
+<div>
+<a href="https://www.linkedin.com/in/michel-h-245436203/">
+<img src="images/linkedinLogo.png" alt="Profil Linkedin" title="Profil Linkedin" /> 
+</a>
+</div>
+&nbsp&nbsp&nbsp&nbsp&nbsp
+<div>
+<a href="images/cv-michelHoffmann.pdf" download>
+<img src="images/cv.png" alt="Telechargement Cv" title="Telechargement Cv" /> 
+</a>
+
+
+&nbsp&nbsp&nbsp&nbsp&nbsp
+
+<a href="https://github.com/Michelhof1978">
+<img src="images/github.png" alt="Projets Github" title="Projets Github" /> 
+</a>
+</div>
+
+<div>
+  Tous droits réservés
+  </div>
+
+</footer>
+
+<div class="compteur">
+<a href="http://www.mon-compteur.fr"><img src="http://www.mon-compteur.fr/html_c02genv2-74909-2" border="0"/></a>
+</div>
+
+<script src="menu.js"></script>
+</body>
+</html>
