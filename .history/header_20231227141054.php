8554c77895eba789c25710b6a3330718a93de11e
HOFFMANN MICHEL <michel.hof@hotmail.fr>
2024-01-08 19:13:08 +0100

    correction

--- .history/header_20231227141054.php
+++ .history/header_20231227141054.php
@@ -8,6 +8,7 @@
   <meta name="Category" content="internet">
   <title>Cv de Michel Hoffmann</title>
   <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" media="screen" href="https://fontlibrary.org//face/glacial-indifference" type="text/css" />
   <link rel="icon" type="image/png" href="../images/logo.png" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
