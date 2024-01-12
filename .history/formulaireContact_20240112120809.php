939e97cb55aca876880c38c8dd877365b5196bc1
HOFFMANN MICHEL <michel.hof@hotmail.fr>
2024-01-12 12:18:49 +0100

    ajout logo les caravanes

---
+++ .history/formulaireContact_20240112120809.php
@@ -0,0 +1,112 @@
+<?php include("header.php"); ?>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+  // Assurez-vous que le formulaire est soumis en POST
+
+  // Récupération des données du formulaire en les nettoyant
+  $nom = htmlspecialchars($_POST["nom"]);
+  $email = isset($_POST["email"]) ? filter_var($_POST["email"], FILTER_VALIDATE_EMAIL) : null;
+  $objet = htmlspecialchars($_POST["objet"]);
+  $message = htmlspecialchars($_POST["message"]);
+
+  // Vérifiez si l'adresse e-mail est vide
+  if ($email === null) {
+    echo '<p class="alert alert-danger ms-5 mt-3 fw-bold">Email invalide</p>';
+  } else {
+    $message = "Message envoyé de :\nNom : $nom\nEmail : $email\nObjet : $objet\nMessage : $message";
+
+    // Envoi de l'e-mail
+    $retour = mail("michel.hof@hotmail.fr", $objet, $message, "From: contact@cvmichel-hoffmann.fr" . "\r\n" . "Reply-to: $email");
+
+    if ($retour) {
+      echo '<p class="alert alert-success mt-3 fw-bold">L\'email a bien été envoyé</p>';
+    } else {
+      echo '<p class="alert alert-danger ms-5 mt-3 fw-bold">Erreur lors de l\'envoi de l\'email</p>';
+    }
+  }
+}
+?>
+
+<!-- Corps du site -->
+
+  <form class="needs-validation" id="myForm" onsubmit="return validateContactForm()" novalidate action="#" method="POST">    <fieldset class="mb-5 ms-2 me-2">
+
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-6">
+
+                <div class="row mb-4">
+                    <div class="col">
+                        <div class="form-outline">
+                            <label for="firstName" class="form-label">Prénom</label>
+                            <input name="firstName" type="text" id="firstName" class="form-control" placeholder="Prénom" required>
+                            <div class="invalid-feedback">
+                                Veuillez saisir votre prénom.
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div class="form-outline">
+                            <label for="lastName" class="form-label">Nom</label>
+                            <input name="lastName" type="text" id="lastName" class="form-control" placeholder="Nom" required>
+                            <div class="invalid-feedback">
+                                Veuillez saisir votre nom.
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                    <div class="form-outline mb-4">
+    <label for="phoneNumber" class="form-label">Téléphone</label>
+    <input name="phoneNumber" type="tel" id="phoneNumber" class="form-control" placeholder="Téléphone" pattern="[0-9]{10,15}" required>
+    <div class="invalid-feedback">
+        Veuillez saisir un numéro de téléphone valide (au moins 10 chiffres).
+    </div>
+                    </div>
+                </div>
+
+                <div class="form-outline mb-4">
+    <label for="email" class="form-label">Adresse Email</label>
+    <div class="input-group has-validation">
+        <input name="email" type="email" id="email" class="form-control" placeholder="Email" required pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.(com|fr)$">
+        <div class="invalid-feedback">
+            Veuillez saisir une adresse email valide avec un domaine .com ou .fr.
+        </div>
+    </div>
+</div>
+
+<div class="form-group">
+                    <label for="message" class="mb-2">Message</label>
+    <div class="form-floating">
+        <textarea name="message" class="form-control" id="message" required></textarea>
+        <label for="message">Votre Message</label>
+        <div class="invalid-feedback">
+            Veuillez saisir votre message.
+        </div>
+    </div>
+</div>
+
+                     <!-- Case à cocher RGPD -->
+                     <div class="form-check mb-4 mt-3">
+    <input class="form-check-input" type="checkbox" id="rgpdCheckbox" name="rgpdCheckbox">
+    <label class="form-check-label" for="rgpdCheckbox">
+        J'accepte que mes données personnelles soient traitées conformément à la politique de confidentialité.
+    </label>
+    <div class="invalid-feedback" id="rgpdError" style="display: none;">
+        Vous devez accepter la politique de confidentialité.
+    </div>
+</div>
+
+                    <div class="g-recaptcha m-4" data-sitekey="6Ld72FwnAAAAABXBamvH-_h6-dyX_phTGFlAWCgR"></div>
+
+                    <button type="submit" value="Valider" id="send-data" class="btn btn-primary btn-block mb-4">
+                        Envoyez
+                    </button>
+                </div>
+            </div>
+        </fieldset>
+    </form>
+    
+<!-- Footer du site -->
+<?php include("footer.php"); ?>
---
+++ .history/index_20240112115522.php
@@ -0,0 +1,113 @@
+<?php include("header.php") ?>
+<!--Corps du site-->
+<div class="corps">
+<div class="text-center mt-5">
+
+    <a href="https://lescaravanesdelabesbre.fr/" target="_blank">
+        <img src="images/logoLesCaravanesDeLaBesbre.png" alt="Réalisation Client" class="logoPal img-fluid" style="width: 15%; margin-top: 50px; margin">
+    </a>
+    <p class="fw-bold">Dernier Projet Client Réalisé de A à Z</p>
+</div>
+
+  <br>
+  <div class="text lead bg-light rounded shadow p-4 pb-5 bg-white rounded ms-1 me-1 ">
+
+    <h2 class="colorBlueDark size40 borderBottomDark">Synthèse</h2>
+
+    <p class="">
+      En Développement web, comme en tout, les connaissances ne sont rien,
+      sans curiosité, intelligence, réflexion et imagination.<br>
+      C’est la curiosité et la passion pour ce métier qui m’ont amené à une
+      reconversion professionnelle et à reprendre mes études.<br>
+      De nature autodidacte et toujours motivé, je mettrai à profit mes
+      expériences pour répondre au mieux à l’exigence du client.<br> Je suis flexible
+      quant à mes déplacements pour satisfaire aux exigences de l'emploi.<br>
+      Je possède naturellement le sens du contact, je suis à l'aise pour travailler
+      en équipe ou en autonomie et prêt à relever de nouveaux challenges.
+    </p>
+
+    <h2 class="colorBlueDark size40 borderBottomDark">Domaine de compétences</h2>
+
+    <br>
+
+    <div class="domaineCompetence">
+      <table>
+        <tr>
+          <th class="colorBlueLight">Compétences</th>
+          <th class="colorBlueLight">Détail</th>
+          <th class="colorBlueLight">Niveau</th>
+        </tr>
+
+        <tr>
+          <td>Management</td>
+          <td>Gestion d'équipe</td>
+          <td>Confirmé</td>
+        </tr>
+
+        <tr>
+          <td>Programation Web</td>
+          <td>Php/Symfony, HTML5, CSS3/Bootstrap, Javascript/React, MySQL, Référencement SEO, Marketing Digital, Logo et Maquette, Git Et Github</td>
+          <td>Confirmé</td>
+        </tr>
+
+        <tr>
+          <td>Anglais</td>
+          <td>Lire et écrire</td>
+          <td>Scolaire</td>
+        </tr>
+
+        <tr>
+          <td>Allemand</td>
+          <td>Lire et écrire</td>
+          <td>Scolaire</td>
+        </tr>
+
+      </table>
+    </div>
+    <br>
+
+
+    <h2 class="colorBlueDark size40 borderBottomDark">Formation</h2>
+
+    <div>
+      Diplôme de niveau 5 : Développement Web Full Stack <br>
+      <div class="activity">Wild Code School 09/2021 à 12/2023<br>
+        <hr>
+      </div>
+
+      BAC : Restauration <br>
+      <div class="activity">Sévigné - Gap 09/1998 – 07/1999<br>
+        <hr>
+      </div>
+
+      CAP / BEP : Restauration <br>
+      <div class="activity">Sévigné - Gap 09/1996 – 07/1998<br>
+        <hr>
+      </div>
+
+      BEPC <br>
+      <div class="activity">Fernand Greg - Champagne/Seine 09/1995 – 07/1996</div>
+
+
+    </div>
+    <br>
+
+    <h2 class="colorBlueDark size40 borderBottomDark">Activités personnelles</h2>
+
+    <div>
+      Ecologie
+      <div class="activity"> Militant et respect de la nature</div>
+      Entraide, Associations
+      <div class="activity">Aide aux plus démunis</div>
+      Randonnées
+      <div class="activity">Marche + Vélo</div>
+      Musique
+      <div class="activity">Ecoute + Concert</div>
+      Surf
+      <div class="activity">Océan</div>
+    </div>
+
+  </div>
+</div>
+
+<?php include("footer.php") ?>
