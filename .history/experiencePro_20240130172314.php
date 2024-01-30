374148f848eac14cdd773ffeecaffeca879c84cf
HOFFMANN MICHEL <michel.hof@hotmail.fr>
2024-01-30 17:35:57 +0100

    modif cv

---
+++ .history/experiencePro_20240130172314.php
@@ -0,0 +1,108 @@
+<?php include("header.php") ?>
+<br>
+<!--Corps du site-->
+<div class="corps size20 text lead">
+
+    <div class="gauche col1 colorBlueLight lead">
+        <h2 class="borderBottomDark ">Harmony Digital</h2>
+        <div class="lead">09/2023 - Aujourd'hui </div>
+    </div>
+
+    <div class="gauche col2 colorBlueDark lead">
+        <h2 class="borderBottomDark">Développeur Web Full-Stack</h2>
+        <div class="lead">
+            -Maquettage, Cahier Des charges, Users Stories...<br>
+            -Base De Données<br>
+            -Back-end Et Front End<br>
+            -Mise En Ligne<br>
+            -Référencement<br>
+        </div>
+        <br />
+    </div>
+
+
+    <div class="gauche col1 colorBlueLight lead">
+        <h2 class="borderBottomDark ">Psa</h2>
+        <div class="lead">07/2006 - 06/2022 </div>
+    </div>
+
+    <div class="gauche col2 colorBlueDark lead">
+        <h2 class="borderBottomDark">Conducteur d'installation</h2>
+        <div class="lead">
+            -Gestion d'un parc matière<br>
+            -Respect de la réglementation en matière de sécurité, des exigences de contrat, des politiques d'entreprise et des procédures opérationnelles
+        </div>
+        <br />
+    </div>
+
+    <div class="gauche col1 colorBlueLight lead">
+        <h2 class="borderBottomDark ">Bigbang</h2>
+        <div class="lead">04/2008 - 12/2016 </div>
+    </div>
+
+    <div class="gauche col2 colorBlueDark lead">
+        <h2 class="borderBottomDark ">Dj, Animateur et gérant de société</h2>
+        <div class="lead">
+            -Organisation et animation d'évènements<br>
+            -relations clients
+        </div>
+        <br />
+    </div>
+
+    <div class="gauche col1 colorBlueLight lead">
+        <h2 class="borderBottomDark">Tendance</h2>
+        <div class="lead">03/2003 - 07/2006 </div>
+    </div>
+
+    <div class="gauche col2 colorBlueDark lead">
+        <h2 class="borderBottomDark ">Responsable de salle</h2>
+        <div class="lead">
+            -Satisfaction des clients grâce à une mise en place immédiate afin de prendre rapidement les commandes de boissons et de répondre à tous leurs besoins<br>
+            -Responsabilités complètes d'ouverture, de fermeture et de changement d'équipe pour favoriser l'efficacité du restaurant et des équipes afin de répondre aux besoins des clients<br>
+            -Applications des procédures adéquates de nettoyage, d'hygiène et de manipulation des aliments dans le respect des réglementations du ministère de la Santé<br>
+            -Présentation au moment opportun auprès des clients pour prendre les commandes et confirmer leur satisfaction par rapport aux repas après le service, en veillant à rectifier tout problème éventuel
+
+        </div>
+        <br />
+    </div>
+
+    <div class="gauche col1 colorBlueLight lead">
+        <h2 class="borderBottomDark ">Shine</h2>
+        <div class="lead">04/2002 - 08/2004 </div>
+    </div>
+
+    <div class="gauche col2 colorBlueDark lead">
+        <h2 class="borderBottomDark ">Responsable événementiel</h2>
+        <div>
+            -Gestion d'une équipe, planning de 12 personnes <br>
+            -Organisation d'évènements<br>
+            -Supervision de l'embauche, de la formation et de l'évolution professionnelle de chaque employé<br>
+            -Relations clients
+
+        </div>
+        <br />
+    </div>
+
+    <div class="gauche col1 colorBlueLight">
+        <h2 class="borderBottomDark ">Pino</h2>
+        <div>09/1999 - 04/2002 </div>
+    </div>
+
+    <div class="gauche col2 colorBlueDark">
+        <h2 class="borderBottomDark">Chef de rang et maître d'hôtel </h2>
+        <div>
+            -Gestion d'une vingtaine de salariés, supervision de l'embauche, de la formation et de l'évolution professionnelle de chaque salarié<br>
+            -Satisfaction du client grâce à une mise en place immédiate afin de prendre rapidement les commandes de boissons et de répondre à tous leurs besoins<br>
+            -A épauler le directeur d'exploitation dans le cadre des décisions opérationnelles quotidiennes
+
+
+        </div>
+        <br />
+    </div>
+
+</div>
+
+<div class="clear"></div>
+<!--Footer du site-->
+
+<?php include("footer.php") ?>
---
+++ .history/index_20240130164120.php
@@ -0,0 +1,109 @@
+<?php include("header.php") ?>
+<!--Corps du site-->
+<div class="corps">
+<div class="text-center mt-5">
+
+    <a href="https://lescaravanesdelabesbre.fr/" target="_blank">
+        <img src="images/logoLesCaravanesDeLaBesbre.png" alt="Réalisation Client" class="logoPal img-fluid" style="width: 15%; margin-top: 50px; margin-bottom: -70px;">
+    </a>
+    <p class="fw-bold">Dernier Projet Client Réalisé de A à Z</p>
+</div>
+
+  <br>
+  <div class="text lead bg-light rounded shadow p-4 pb-5 bg-white rounded ms-1 me-1 ">
+
+    <h2 class="colorBlueDark size40 borderBottomDark">Synthèse</h2>
+
+    <p class="">
+      &nbsp; &nbsp; &nbsp; &nbsp; Passionné par le développement web, je suis constamment en ébullition d'idées que je m'efforce de concrétiser. <br>
+       Mon intérêt marqué pour l'aspect créatif de ce domaine m'a motivé à entreprendre une reconversion professionnelle et à reprendre mes études. <br>
+       Autodidacte de nature et toujours avide d'apprendre, je tire profit de mes expériences pour répondre au mieux aux exigences des clients.<br> Flexible quant aux déplacements, je suis prêt à relever de nouveaux défis, que ce soit en équipe ou en autonomie. <br>
+       Mon sens du contact naturel complète mes compétences techniques, faisant de moi un professionnel adaptable et déterminé.
+       <strong></strong>Je suis reconnu RQTH permanent qui ne  pas un poste aménagé)
+      </p>
+
+    <h2 class="colorBlueDark size40 borderBottomDark">Domaine de compétences</h2>
+
+    <br>
+
+    <div class="domaineCompetence">
+      <table>
+        <tr>
+          <th class="colorBlueLight">Compétences</th>
+          <th class="colorBlueLight">Détail</th>
+          <th class="colorBlueLight">Niveau</th>
+        </tr>
+
+        <tr>
+          <td>Management</td>
+          <td>Gestion d'équipe</td>
+          <td>Confirmé</td>
+        </tr>
+
+        <tr>
+          <td>Programation Web</td>
+          <td>Php/Symfony, HTML5, CSS3/Bootstrap, Javascript/React, MySQL, Référencement SEO, Marketing Digital, Logo et Maquette, Git Et Github</td>
+          <td>Confirmé</td>
+        </tr>
+
+        <tr>
+          <td>Anglais</td>
+          <td>Lire et écrire</td>
+          <td>Scolaire</td>
+        </tr>
+
+        <tr>
+          <td>Allemand</td>
+          <td>Lire et écrire</td>
+          <td>Scolaire</td>
+        </tr>
+
+      </table>
+    </div>
+    <br>
+
+
+    <h2 class="colorBlueDark size40 borderBottomDark">Formation</h2>
+
+    <div>
+      Diplôme de niveau 5 : Développement Web Full Stack <br>
+      <div class="activity">Wild Code School 09/2021 à 12/2023<br>
+        <hr>
+      </div>
+
+      BAC : Restauration <br>
+      <div class="activity">Sévigné - Gap 09/1998 – 07/1999<br>
+        <hr>
+      </div>
+
+      CAP / BEP : Restauration <br>
+      <div class="activity">Sévigné - Gap 09/1996 – 07/1998<br>
+        <hr>
+      </div>
+
+      BEPC <br>
+      <div class="activity">Fernand Greg - Champagne/Seine 09/1995 – 07/1996</div>
+
+
+    </div>
+    <br>
+
+    <h2 class="colorBlueDark size40 borderBottomDark">Activités personnelles</h2>
+
+    <div>
+      Ecologie
+      <div class="activity"> Militant et respect de la nature</div>
+      Entraide, Associations
+      <div class="activity">Aide aux plus démunis</div>
+      Randonnées
+      <div class="activity">Marche + Vélo</div>
+      Musique
+      <div class="activity">Ecoute + Concert</div>
+      Surf
+      <div class="activity">Océan</div>
+    </div>
+
+  </div>
+</div>
+
+<?php include("footer.php") ?>
